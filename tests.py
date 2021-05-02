24b46da3a07cc1554feaa958cd3d253bba9d259c
CL-75 <49925072+CL-75@users.noreply.github.com>
2021-05-02 16:31:55 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,13 @@
+# Name: Casey Levy
+# CS 362 - A2 - Test Driven Development
+# Description: Creating and executing several tests for
+#   check_pwd.py program and implementing Test Driven Development
+
+from check_pwd import check_pwd
+import unittest
+
+# Class for password check tests
+class TestCase(unittest.TestCase):
+
+	def test1(self):
+		
